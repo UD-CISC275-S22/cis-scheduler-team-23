c01f148ec51dcdc3e8be561f9353de01d1d3e349
oceanshen <oshen@udel.edu>
2022-04-05 20:44:57 -0400

    added name - ocean

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                MOT - UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p> Ocean Shen </p>
         </div>
     );
 }
