fa8558943d7d642b1376874142b776f34f08d53d
tommyoves <ovest@udel.edu>
2022-04-22 22:46:56 -0400

    push attempt 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Button, Modal } from "react-bootstrap";
 
 import "./App.css";
 import { DisplayCourses } from "./Components/DisplayCourses";
 
 function App(): JSX.Element {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
         <div className="App">
             <header className="App-header">
                 <p> University of Delaware Computer Science Scheduler </p>
                 <p> Matt Holdorf, Ocean Shen, Tommy Oves </p>
             </header>
-            <p>
-                Hello and welcome to our website to help Computer Science majors
-                at the University of Delaware make their schedules. This website
-                will allow you to put together your desired schedule for the
-                rest of your college career. Add the classes you wish to take to
-                their desired semester, add/remove semesters, and even edit the
-                class itself if necessary!
-            </p>
+            <div>
+                <Button color="black" onClick={handleOpen}>
+                    Click here for how to begin!
+                </Button>
+                <Modal show={open} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Welcome to our Website!</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Hello and welcome to our website to help Computer
+                        Science majors at the University of Delaware make
+                        schedules. This website will allow you to put together
+                        your desired schedule for the rest of your career. Add
+                        the classes you wish to take to their their semester,
+                        add/remove semesters, add one or more plans, and even
+                        edit the class itself if necessary!
+                    </Modal.Body>
+                    <Modal.Footer></Modal.Footer>
+                </Modal>
+            </div>
             <Col>
                 <DisplayCourses></DisplayCourses>
             </Col>
