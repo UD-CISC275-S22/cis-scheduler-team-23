7df44487a98385168d3f030e923128c8be485611
oceanshen <oshen@udel.edu>
2022-05-16 21:20:24 -0400

    forgot to make branch so pushing now

---
+++ src/Tests/EditPlan.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("Add Course Component Tests", () => {
+    beforeEach(() => {
+        render(<App />);
+    });
+
+    test("Test for the cancel button within the Add Plan Modal", () => {
+        const editPlanButton = screen.getByTestId("editPlanButton");
+        editPlanButton.click();
+
+        const editPlanModal = screen.getByText("Enter a new plan name:");
+        expect(editPlanModal).toBeInTheDocument();
+
+        const cancelButton = screen.getByTestId("editPlanCancelButton");
+        cancelButton.click();
+    });
+
+    test("Test for the save changes button within the Add Plan Modal", () => {
+        const editPlanButton = screen.getByTestId("editPlanButton");
+        editPlanButton.click();
+
+        const editPlanModal = screen.getByText("Enter a new plan name:");
+        expect(editPlanModal).toBeInTheDocument();
+
+        const saveChangesButton = screen.getByTestId(
+            "editPlanSaveChangesButton"
+        );
+        saveChangesButton.click();
+    });
+});
