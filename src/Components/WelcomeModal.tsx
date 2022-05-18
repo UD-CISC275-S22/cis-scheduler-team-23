a3b31d45ac24a5e02e1fd97b18b4f64da0ae03f8
tommyoves <ovest@udel.edu>
2022-05-17 20:59:10 -0400

    modal changed

--- src/Components/WelcomeModal.tsx
+++ src/Components/WelcomeModal.tsx
@@ -2,7 +2,7 @@ import { Button, Modal } from "react-bootstrap";
 import React, { useState } from "react";
 
 export function WelcomeModal() {
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(true);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
@@ -16,14 +16,29 @@ export function WelcomeModal() {
                     <Modal.Title>Welcome to our Website!</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    Hello and welcome to our website to help Computer Science
-                    majors at the University of Delaware make schedules. This
-                    website will allow you to put together your desired schedule
-                    for the rest of your career. Add the classes you wish to
-                    take to their their semester, add/remove semesters, add one
-                    or more plans, and even edit the class itself if necessary!
+                    Hello and welcome to our interactive website to help
+                    Computer Science majors at the University of Delaware make
+                    their schedules. You will be given a plan to work inside,
+                    where you can add/remove semesters and the classes you wish
+                    to take in each of those semesters. There are many options
+                    for you to edit the plan exactly how you like, including
+                    even being able to edit the courses themselves if you would
+                    like. Also, if you look at the column on the right side of
+                    the website, you can change the concentration to what you
+                    wish, and tables of courses and requirements will be
+                    displayed. This allows you to see exactly what you need to
+                    finish your degree for Computer Science at the University of
+                    Delaware. Good luck!
                 </Modal.Body>
-                <Modal.Footer></Modal.Footer>
+                <Modal.Footer>
+                    <img
+                        src="https://media2.giphy.com/media/VxXL8GwKOei90gYlXC/giphy.gif?cid=ecf05e47xhc4mltmj5joywckpdw9y2zlkzfmv9j3bqh971to&rid=giphy.gif&ct=s"
+                        width="30"
+                        onClick={() =>
+                            window.open("https://www.udel.edu", "_blank")
+                        }
+                    />
+                </Modal.Footer>
             </Modal>
         </div>
     );
