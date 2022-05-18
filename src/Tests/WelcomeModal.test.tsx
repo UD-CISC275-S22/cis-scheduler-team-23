143cc4b1d5fd687083021fab2d4e1c80729c352c
oceanshen <oshen@udel.edu>
2022-05-17 22:04:13 -0400

    changed welcome modal button name

--- src/Tests/WelcomeModal.test.tsx
+++ src/Tests/WelcomeModal.test.tsx
@@ -10,14 +10,14 @@ describe("Welcome Modal Tests", () => {
     test("Click for how to begin button works", () => {
         expect(
             screen.getByRole("button", {
-                name: /Click here for how to begin!/i
+                name: /Click here instructions!/i
             })
         ).toBeInTheDocument();
     });
 
     test("Clicking the 'click here to begin button' shows a welcome message", () => {
         const changeTypeButton = screen.getByRole("button", {
-            name: /Click here for how to begin!/i
+            name: /Click here instructions!/i
         });
         changeTypeButton.click();
         const beginHereText = screen.getByText(/Welcome to our Website!/i);
